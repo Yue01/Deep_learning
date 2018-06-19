9a9284f1c39b4ee52cad2ddb07fcc6869cdf1148
Yue01 <434907471@qq.com>
2018-06-18 22:46:53 -0700

    week_1

--- Part_1/week_1.py
+++ Part_1/week_1.py
@@ -1,3 +1,9 @@
+"""
+week_1: numpy基础和logistic 回归
+
+"""
+
+
 # basic_sigmoid
 
 import math
@@ -18,3 +24,189 @@ def sigmoid_derivative(x):
 	ds= s*(1-s)
 
 	return ds
+
+#Graded Function: image2vector
+def image2vector(image):
+    """
+    Argument:
+    image -- a numpy array of shape (length, height, depth)
+
+    Returns:
+    v -- a vector of shape (length*height*depth, 1)
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    v = image.reshape((image.shape[0] * image.shape[1] * image.shape[2], 1))
+    ### END CODE HERE ###
+
+    return v
+# This is a 3 by 3 by 2 array, typically images will be (num_px_x, num_px_y,3) where 3 represents the RGB values
+image = np.array([[[ 0.67826139,  0.29380381],
+        [ 0.90714982,  0.52835647],
+        [ 0.4215251 ,  0.45017551]],
+
+       [[ 0.92814219,  0.96677647],
+        [ 0.85304703,  0.52351845],
+        [ 0.19981397,  0.27417313]],
+
+       [[ 0.60659855,  0.00533165],
+        [ 0.10820313,  0.49978937],
+        [ 0.34144279,  0.94630077]]])
+
+print ("image2vector(image) = " + str(image2vector(image)))
+
+def normalizeRows(x):
+	
+	x_norm = np.linalg.norm(x, axis=1, keepdims = True)  #计算每一行的长度，得到一个列向量
+
+	#计算每一行的长度，得到一个列向量
+    x = x / x_norm  
+    # 广播机制
+
+    return x
+
+    # GRADED FUNCTION: softmax
+
+def softmax(x):
+
+    x_exp = np.exp(x) # (n,m)
+
+    x_sum = np.sum(x_exp, axis = 1, keepdims = True) # (n,1)
+
+    s = x_exp / x_sum  
+
+
+    return s
+
+
+"""
+
+logistic Regression
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+index = 25
+plt.imshow(train_set_x_orig[index])
+print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+print ("Number of training examples: m_train = " + str(m_train))
+print ("Number of testing examples: m_test = " + str(m_test))
+print ("Height/Width of each image: num_px = " + str(num_px))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_set_x shape: " + str(train_set_x_orig.shape))
+print ("train_set_y shape: " + str(train_set_y.shape))
+print ("test_set_x shape: " + str(test_set_x_orig.shape))
+print ("test_set_y shape: " + str(test_set_y.shape))
+
+"""
+
+Number of training examples: m_train = 209
+Number of testing examples: m_test = 50
+Height/Width of each image: num_px = 64
+Each image is of size: (64, 64, 3)
+train_set_x shape: (209, 64, 64, 3)
+train_set_y shape: (1, 209)
+test_set_x shape: (50, 64, 64, 3)
+test_set_y shape: (1, 50)
+
+"""
+
+train_set_x_flatten = train_set_x_orig.reshape(m_train, -1).T
+test_set_x_flatten = test_set_x_orig.reshape(m_test, -1).T
+
+
+print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print ("train_set_y shape: " + str(train_set_y.shape))
+print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print ("test_set_y shape: " + str(test_set_y.shape))
+print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
+
+
+"""
+
+Common steps for pre-processing a new dataset are: 
+- Figure out the dimensions and shapes of the problem (m_train, m_test, num_px, …) 
+- Reshape the datasets such that each example is now a vector of size (num_px * num_px * 3, 1) 
+- “Standardize” the data
+
+"""
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+    """
+    Builds the logistic regression model by calling the function you've implemented previously
+
+    Arguments:
+    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
+    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
+    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
+    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
+    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
+    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
+    print_cost -- Set to true to print the cost every 100 iterations
+
+    Returns:
+    d -- dictionary containing information about the model.
+    """
+
+    ### START CODE HERE ###
+
+    # initialize parameters with zeros (≈ 1 line of code)
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+
+    # Predict test/train set examples (≈ 2 lines of code)
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    ### END CODE HERE ###
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
+
+
+
+
+
+
+
+
+
+
